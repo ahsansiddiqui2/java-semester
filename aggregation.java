77931ba9efd235be0e33604a3ff5725cbade5c6b
ahsansiddiqui2 <04611379+ahsansiddiqui2@users.noreply.github.com>
2022-08-03 23:17:48 +0500

    homework

---
+++ aggregation.java
@@ -0,0 +1,28 @@
+class Human{
+	laptop mine;
+	airpods my;
+	bag blue;
+	SectionII Myclass;
+}
+
+class laptop{
+	String name = "Predator";
+	int ram = 32;
+	int chips = 04;
+}
+class airpods{
+	String company = "Samsung";
+	int quantity = 02;
+	String quality = "Good";
+}
+class bag{
+	int num_of_pens = 03;
+	int num_of_copies = 1;
+	int num_of_books = 1;
+}
+class SectionII{
+	int num_of_lights = 18;
+	int num_of_fans = 12;
+	int NumofBoard = 1;
+
+}
